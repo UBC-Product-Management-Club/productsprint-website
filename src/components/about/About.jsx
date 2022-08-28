141af92357a92ba5ae3b522df8f4d41eaceeeafb
Kevin Tan <kevin2012tan@gmail.com>
2022-08-28 15:22:06 -0700

    created about component and aligned items to center

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import AboutImg from '../../assets/about_img.png';
+import './about.css';
 
 function About() {
   return (
-    <div className='container'>
-      <div className='about_container'>
+    <div className='container about_container'>
+      <div>
         <p className='title'>WHO WE ARE</p>
+      </div>
+      <div>
         <p className='about_text'>
           A community of aspiring product managers that comes together to learn
           skills, meet with industry, and prepare for careers in product
-          management. Meet the team →
+          management.{' '}
+          <a className='about_link' href=''>
+            Meet the team →
+          </a>
         </p>
+      </div>
+      <div>
         <img className='about_img' src={AboutImg} alt='' />
       </div>
     </div>
