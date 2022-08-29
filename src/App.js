aab38a494d7ddeba82ce7042f73dda12af0b2a0d
Kevin Tan <kevin2012tan@gmail.com>
2022-08-29 10:20:41 -0700

    added club highlights section and removed universal styling that centers items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import About from './components/about/About';
 import Program from './components/program/Program';
+import Highlights from './components/highlights/Highlights';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Program />
+      <Highlights />
     </>
   );
 }
---
+++ src/components/highlights/Highlights.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './highlights.css';
+
+function Highlights() {
+  return (
+    <div className='container'>
+      <div>
+        <p className='title'>CLUB HIGHLIGHTS</p>
+        <div className='highlights_container'>
+          <p className='subtitle'>BUILDING A COMMUNITY</p>
+          <p>What we’ve accomplished since our founding in 2020:</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Highlights;
