71dbf996e438d54f7385d0d137a1cba9970db131
Kevin Tan <kevin2012tan@gmail.com>
2022-08-29 10:32:51 -0700

    created club highlights section

--- src/components/highlights/Highlights.jsx
+++ src/components/highlights/Highlights.jsx
@@ -6,10 +6,37 @@ function Highlights() {
     <div className='container'>
       <div>
         <p className='title'>CLUB HIGHLIGHTS</p>
-        <div className='highlights_container'>
+        <div>
           <p className='subtitle'>BUILDING A COMMUNITY</p>
           <p>What we’ve accomplished since our founding in 2020:</p>
         </div>
+        <div className='achievements'>
+          <p>
+            <span className='achievement_statistics'>10+ </span> workshop
+            <br />
+            led by industry PM’s
+          </p>
+          <p>
+            <span className='achievement_statistics'>70+</span>
+            mentors from top companies
+          </p>
+          <p>
+            <span className='achievement_statistics'>Largest</span> student-led
+            product competition in Canada
+          </p>
+          <p>
+            <span className='achievement_statistics'>300+ </span>
+            students
+            <br /> in our community
+          </p>
+          <p>
+            <span className='achievement_statistics'>$4,500+</span>
+            <br /> prizes awarded{' '}
+          </p>
+        </div>
+        <a className='learn_more_link' href=''>
+          Learn more →
+        </a>
       </div>
     </div>
   );
