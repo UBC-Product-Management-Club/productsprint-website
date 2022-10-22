8522bc73905dc754b8139e821674e688b0a3de2b
Kevin Tan <kevin2012tan@gmail.com>
2022-10-22 00:21:16 -0700

    fellow project page

--- src/components/shared/team/Projects.jsx
+++ src/components/shared/team/Projects.jsx
@@ -6,16 +6,18 @@ import ProjectCard from './ProjectCard';
 
 function Projects({ img, name, title, bio_text, linkedin, projects }) {
   return (
-    <div>
+    <div className='fellow_project_container'>
       <div className='bio_container'>
-        <img src={img} alt='img' />
-        <div className='bio_text_container'>
-          <p className='bio_name'>{name}</p>
-          <p className='bio_title'>{title}</p>
-          <p className='bio_text'>{bio_text}</p>
-          <a className='about_link' target='blank' href={linkedin}>
-            Visit LinkedIn Profile →
-          </a>
+        <div className='bio_information_container'>
+          <img src={img} alt='img' />
+          <div className='bio_text_container'>
+            <p className='bio_name'>{name}</p>
+            <p className='bio_title'>{title}</p>
+            <p className='bio_text'>{bio_text}</p>
+            <a className='about_link' target='blank' href={linkedin}>
+              Visit LinkedIn Profile →
+            </a>
+          </div>
         </div>
       </div>
       <div className='projects_container'>
