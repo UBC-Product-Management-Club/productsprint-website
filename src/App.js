fded529a2f7e2d18f142ed15c3c045adaa14dcfe
Kevin Tan <kevin2012tan@gmail.com>
2023-06-26 19:41:15 -0700

    added linting and fixed some naming and imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
