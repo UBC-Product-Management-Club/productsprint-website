a3e2a19c20d5cd09881d2ac4768227d2801fd114
Minh Au <minhau@student.ubc.ca>
2023-07-25 09:03:02 -0700

    Setup Tailwindcss (#4)
    * add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter' : ['Inter', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        'h1' : ['3.75rem'],
+        'h2' : ['1.5rem'],
+        'h3' : ['1.18rem'],
+        'p' : ['0.75rem'],
+        'button' : ['1.25rem'],
+      },
+      fontWeight: {
+        'header' : ['700'],
+        'content': ['200'],
+      }
+    },
+  },
+  plugins: [],
+}
