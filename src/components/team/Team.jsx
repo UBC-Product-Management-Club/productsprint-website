806dc2719e62ef070a2ab8e01a87872fb9084cbc
Minh Au <auduongminh2004@gmail.com>
2023-07-30 13:22:40 -0700

    add team carousel

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -3,6 +3,7 @@ import ".//team.css";
 import ProfileCard from "../shared/team/ProfileCard";
 import { client } from "../utils/apiClient";
 import TransparentButton from "../shared/buttons/TransparentButton";
+import { MdOutlineArrowForwardIos, MdOutlineArrowBackIos} from "react-icons/md";
 
 async function getExecs() {
   try {
@@ -17,7 +18,11 @@ async function getExecs() {
 function Team() {
   const [executives, setExecutives] = useState([])
   const [currDp, setCurrDp] = useState("Leads") // Default value 
+  const [currIdx, setCurrIdx] = useState(0)
+  const [currExecs, setCurrExecs] = useState([])
+
   const departments = ["Leads", "Partnerships", "Finance", "Marketing", "Tech", "Events", "Advisor", "Others"]
+  const maxDisplayItem = 6
 
   const camelize = (s) => s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase()
   const processExec = (ex) => {
@@ -26,6 +31,17 @@ function Team() {
       let newDp = departments.filter((d) => d.includes(ex.department) | ex.department.includes(d))
       ex.department = newDp.length > 0 ? newDp[0] : ex.department
     }
+  }
+  const arrowHandler = (inc) => {
+    if (inc > 0) {
+      (currIdx + inc) * maxDisplayItem < currExecs.length ? setCurrIdx(currIdx + inc) : null
+    } else if (inc < 0) {
+      (currIdx + inc) * maxDisplayItem >= 0 ? setCurrIdx(currIdx + inc) : null
+    }
+  }
+  const dpButtonHanler = (d) => {
+    setCurrDp(d)
+    setCurrIdx(0)
   }
 
   useEffect(() => {
@@ -36,6 +52,10 @@ function Team() {
     })()
   }, [])
 
+  useEffect(() => {
+    setCurrExecs(executives.filter((exec) => (exec.department == currDp) || (currDp == "Others" && !departments.includes(exec.department))))
+  }, [currDp, executives])
+
   return (
     <div>
       <div className="flex flex-col items-center gap-[2.9rem]">
@@ -44,22 +64,26 @@ function Team() {
         <div className="flex gap-[1.26rem]">
           {departments.map((d) => 
             <TransparentButton disabled={d.toLowerCase() == currDp.toLowerCase()} 
-              onClick={() => setCurrDp(d)}>
+              onClick={() => dpButtonHanler(d)}>
                 {d}
             </TransparentButton>)}
         </div>
-        <div className="card_display_container">
-          {
-            executives.filter((exec) => (exec.department == currDp) || (currDp == "Others" && !departments.includes(exec.department))).map((exec, _) => {
-              return (
-                <ProfileCard
-                  img={exec.img}
-                  name={exec.name}
-                  title={exec.title}
-                ></ProfileCard>
-              )
-            })
-          }
+        <div className="w-[68.6rem] h-[49.6rem] flex flex-row content-start px-[1.6rem] py-[2.5rem] items-center">
+          <MdOutlineArrowBackIos className="w-[2rem] h-[3.6rem] shrink-0 hover:cursor-pointer" onClick={() => arrowHandler(-1)}/>
+            <div className="w-full h-full flex flex-wrap gap-x-[5rem] gap-y-[2.5rem] content-start ml-[4.2rem]">
+            {
+              currExecs.slice(currIdx * maxDisplayItem, (currIdx + 1) * maxDisplayItem).map((exec, _) => {
+                return (
+                  <ProfileCard
+                    img={exec.img}
+                    name={exec.name}
+                    title={exec.title}
+                  ></ProfileCard>
+                )
+              })
+            }
+          </div>
+         <MdOutlineArrowForwardIos className="w-[2rem] h-[3.6rem] shrink-0 hover:cursor-pointer" onClick={() => arrowHandler(1)}/>
         </div>
       </div>
     </div>
