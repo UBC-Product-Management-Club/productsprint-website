05c8f4345ca2b59d4657bbe46f7df9137323069b
Kevin Tan <kevin2012tan@gmail.com>
2023-08-28 21:46:50 -0700

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import AboutImg from '../../assets/about_img.png';
-import './about.css';
 
 function About() {
   return (
-    <div className='container about_container'>
+    <div className='mx-[2.19rem] mt-[7.5rem]'>
+      <p className='title'>WHO WE ARE</p>
       <div>
-        <p className='title'>WHO WE ARE</p>
-      </div>
-      <div className='about_text_container'>
-        <p className='about_text'>
+        <p className='mt-[1.37rem]'>
           A community of aspiring product managers that comes together to learn
           skills, meet with industry, and prepare for careers in product
           management.
           <br />
           <br />
         </p>
-        {/* <a className='about_link' href=''>
+        <a className='text-[#80BDFF]' href=''>
           Meet the team →
-        </a> */}
+        </a>
       </div>
-      <div>
+      <div className='hidden'>
         <img className='about_img' src={AboutImg} alt='' />
       </div>
     </div>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,25 +6,29 @@ import ScrollDown from '../../assets/scroll_down.png';
 
 function Hero() {
   return (
-    <div className='container'>
-      <div className='hero_container'>
-        <div className='hero_text max-w-[19.5rem]'>
-          <h1 className='text-[2.75rem] font-[600]'>Explore product</h1>
-          <h1 className='text-[2.75rem] font-[600]'>management with us.</h1>
-          <div className='mt-[3.75rem] space-y-[10rem]'>
-            <a href='https://tally.so/r/mODJXk'>
-              <PrimaryButton className='primary'>Join the club</PrimaryButton>
-            </a>
-            <a href='https://tally.so/r/wobY8e'>
-              <SecondaryButton className='mt-[1.94rem]'>Volunteer as a PM</SecondaryButton>
-            </a>
-          </div>
+    <div className='mx-[2.19rem]'>
+      <div className='hero_text max-w-[19.5rem]'>
+        <h1 className='text-[2.75rem] font-[600]'>Explore product</h1>
+        <h1 className='text-[2.75rem] font-[600]'>management with us.</h1>
+        <div className='mt-[3.75rem] space-y-[10rem]'>
+          <a href='https://tally.so/r/mODJXk'>
+            <PrimaryButton className='primary'>Join the club</PrimaryButton>
+          </a>
+          <a href='https://tally.so/r/wobY8e'>
+            <SecondaryButton className='mt-[1.94rem]'>
+              Volunteer as a PM
+            </SecondaryButton>
+          </a>
         </div>
-        <img className='hidden' src={HeroImg} alt='hero' />
       </div>
+      <img className='hidden' src={HeroImg} alt='hero' />
       <div className='mt-[4.19rem]'>
         <p className='font-[400] color-[#FFF]'>Scroll to learn more</p>
-        <img className='w-[2.5625rem] mt-[1.06rem]' src={ScrollDown} alt='scroll down' />
+        <img
+          className='w-[2.5625rem] mt-[1.06rem]'
+          src={ScrollDown}
+          alt='scroll down'
+        />
       </div>
     </div>
   );
