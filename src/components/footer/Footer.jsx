81c8a1067de4f4b4d90556981a660abd49354cb8
Kevin Tan <kevin2012tan@gmail.com>
2023-08-28 22:06:00 -0700

    what we offer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,56 +1,56 @@
-import React from 'react';
-import './footer.css';
-import FooterLogo from '../../assets/logo.png';
-import { MdEmail } from 'react-icons/md';
-import { BsFacebook } from 'react-icons/bs';
-import { BsInstagram } from 'react-icons/bs';
-import { BsLinkedin } from 'react-icons/bs';
-import { BsYoutube } from 'react-icons/bs';
+// import React from 'react';
+// import './footer.css';
+// import FooterLogo from '../../assets/logo.png';
+// import { MdEmail } from 'react-icons/md';
+// import { BsFacebook } from 'react-icons/bs';
+// import { BsInstagram } from 'react-icons/bs';
+// import { BsLinkedin } from 'react-icons/bs';
+// import { BsYoutube } from 'react-icons/bs';
 
-function Footer() {
-  return (
-    <div>
-      <footer>
-        <div className='logo_container'>
-          <img className='logo' src={FooterLogo} alt='pmc-logo' />
-          <p className='logo_text'> UBC PRODUCT MANAGEMENT CLUB</p>
-        </div>
-        <div className='contacts'>
-          <a className='email_link' href='team@ubcpm.club'>
-            <MdEmail /> team@ubcpm.club
-          </a>
-          <ul className='footer_links'>
-            <li>
-              <a target='_blank' href='https://www.facebook.com/ubcpmc'>
-                <BsFacebook />
-              </a>
-            </li>
-            <li>
-              <a target='_blank' href='https://www.instagram.com/ubcpmc/'>
-                <BsInstagram />
-              </a>
-            </li>
-            <li>
-              <a
-                target='_blank'
-                href='https://www.linkedin.com/company/ubc-product-management-club/'
-              >
-                <BsLinkedin />
-              </a>
-            </li>
-            <li>
-              <a
-                target='_blank'
-                href='https://www.youtube.com/channel/UCkcrUv-e7GndWtFdihI9FGQ/videos'
-              >
-                <BsYoutube className='fa_icons' />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    </div>
-  );
-}
+// function Footer() {
+//   return (
+//     <div>
+//       <footer>
+//         <div className='logo_container'>
+//           <img className='logo' src={FooterLogo} alt='pmc-logo' />
+//           <p className='logo_text'> UBC PRODUCT MANAGEMENT CLUB</p>
+//         </div>
+//         <div className='contacts'>
+//           <a className='email_link' href='team@ubcpm.club'>
+//             <MdEmail /> team@ubcpm.club
+//           </a>
+//           <ul className='footer_links'>
+//             <li>
+//               <a target='_blank' href='https://www.facebook.com/ubcpmc'>
+//                 <BsFacebook />
+//               </a>
+//             </li>
+//             <li>
+//               <a target='_blank' href='https://www.instagram.com/ubcpmc/'>
+//                 <BsInstagram />
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 target='_blank'
+//                 href='https://www.linkedin.com/company/ubc-product-management-club/'
+//               >
+//                 <BsLinkedin />
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 target='_blank'
+//                 href='https://www.youtube.com/channel/UCkcrUv-e7GndWtFdihI9FGQ/videos'
+//               >
+//                 <BsYoutube className='fa_icons' />
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </footer>
+//     </div>
+//   );
+// }
 
-export default Footer;
+// export default Footer;
--- src/components/program/Program.jsx
+++ src/components/program/Program.jsx
@@ -1,74 +1,56 @@
-// import React from 'react';
-// import PrimaryButton from '../shared/buttons/PrimaryButton';
+import React from 'react';
+import PrimaryButton from '../shared/buttons/PrimaryButton';
 // import './/program.css';
-// import ProductSprint from '../../assets/productSprint.png';
-// import Fellowship from '../../assets/fellowship.png';
-// import { Link } from 'react-router-dom';
+import ProductSprint from '../../assets/productSprint.png';
+import Fellowship from '../../assets/fellowship.png';
+import { Link } from 'react-router-dom';
 
-// function Program() {
-//   return (
-//     <div className='container'>
-//       <div className='program_container'>
-//         <div>
-//           <p className='title'>WHAT WE OFFER</p>
-//         </div>
+function Program() {
+  return (
+    <div className='mx-[2.19rem]'>
+      <p className='title mt-[6.25rem]'>WHAT WE OFFER</p>
 
-//         <div className='information_container'>
-//           <img
-//             className='membership_photo'
-//             id='resized_membership'
-//             src={ProductSprint}
-//             alt='membership'
-//           />
-//           <div className='text_container'>
-//             <p className='subtitle'>MEMBERSHIP</p>
-//             <p className='program_text'>
-//               Be part of our community, in-person or online. Members gain access
-//               to events, programs, newsletters, and our online community.
-//               <br></br>
-//               <br></br>
-//               This fall, you can look forward to bi-weekly chats with dozens of
-//               product managers, our fellowship program, and interview
-//               preparation opportunities.
-//             </p>
-//             <div className='button_container'>
-//               <a href='https://tally.so/r/mODJXk'>
-//                 <PrimaryButton className='primary'>
-//                   Become a Member
-//                 </PrimaryButton>
-//               </a>
-//             </div>
-//           </div>
+      <div className='mt-[3rem]'>
+        <img src={ProductSprint} alt='membership' className='rounded-[2rem]' />
+        <div className='mt-[3rem]'>
+          <p className='text-[2.25rem] font-[600]'>MEMBERSHIP</p>
+          <p className='mt-[1.75rem]'>
+            Be part of our community, in-person or online. Members gain access
+            to events, programs, newsletters, and our online community.
+            <br></br>
+            <br></br>
+            This fall, you can look forward to bi-weekly chats with dozens of
+            product managers, our fellowship program, and interview preparation
+            opportunities.
+          </p>
+          <a href='https://tally.so/r/mODJXk'>
+            <PrimaryButton className='primary mt-[1.75rem]'>
+              Become a Member
+            </PrimaryButton>
+          </a>
+        </div>
+      </div>
 
-//           <img
-//             className='membership_photo'
-//             id='normal_screen_membership'
-//             src={ProductSprint}
-//             alt='membership'
-//           />
-//         </div>
+      <div className='mt-[3rem]'>
+        <img src={Fellowship} alt='Fellowship' className='rounded-[2rem]' />
+        <div className='mt-[3rem]'>
+          <p className='text-[2.25rem] font-[600]'>MEMBERSHIP</p>
+          <p className='mt-[1.75rem]'>
+            Whether you are new to product management or you want to step up
+            your game, the projects you work on over the course of the semester
+            will teach you the core PM skills while preparing you for
+            internships. As a bonus, you'll get insight into the industry during
+            regular mentorship with product managers!
+          </p>
+          <Link to='fellowship'>
+            <PrimaryButton className='primary mt-[1.75rem]'>
+              Learn more
+            </PrimaryButton>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-//         <div className='information_container'>
-//           <img className='fellowship_photo' src={Fellowship} alt='fellowship' />
-//           <div className='text_container'>
-//             <p className='subtitle'>FELLOWSHIP</p>
-//             <p className='program_text'>
-//               Whether you are new to product management or you want to step up
-//               your game, the projects you work on over the course of the
-//               semester will teach you the core PM skills while preparing you for
-//               internships. As a bonus, you'll get insight into the industry
-//               during regular mentorship with product managers!
-//             </p>
-//             <div className='button_container'>
-//               <Link to='fellowship'>
-//                 <PrimaryButton className='primary'>Learn more</PrimaryButton>
-//               </Link>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Program;
+export default Program;
