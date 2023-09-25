1eee51a29ceeb8d13360c9fc545854ca3b4c012b
Kevin Tan <kevin2012tan@gmail.com>
2023-09-24 21:00:14 -0700

    fixed formatting

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,12 +18,12 @@ function Footer() {
         <ul className="mt-[2rem] flex gap-[3rem] justify-center items-center">
           <li>
             <a target="_blank" href="https://www.facebook.com/ubcpmc">
-              <BsFacebook className="w-[3rem] h-[3rem]" />
+              <BsFacebook className="w-[2rem] h-[2rem]" />
             </a>
           </li>
           <li>
             <a target="_blank" href="https://www.instagram.com/ubcpmc/">
-              <BsInstagram className="w-[3rem] h-[3rem]" />
+              <BsInstagram className="w-[2rem] h-[2rem]" />
             </a>
           </li>
           <li>
@@ -31,7 +31,7 @@ function Footer() {
               target="_blank"
               href="https://www.linkedin.com/company/ubc-product-management-club/"
             >
-              <BsLinkedin className="w-[3rem] h-[3rem]" />
+              <BsLinkedin className="w-[2rem] h-[2rem]" />
             </a>
           </li>
           <li>
@@ -39,7 +39,7 @@ function Footer() {
               target="_blank"
               href="https://www.youtube.com/channel/UCkcrUv-e7GndWtFdihI9FGQ/videos"
             >
-              <BsYoutube className="w-[3rem] h-[3rem]" />
+              <BsYoutube className="w-[2rem] h-[2rem]" />
             </a>
           </li>
         </ul>
@@ -51,47 +51,6 @@ function Footer() {
         </div>
       </div>
     </footer>
-    // <div>
-    //   <footer>
-    //     <div className='logo_container'>
-    //       <img className='logo' src={FooterLogo} alt='pmc-logo' />
-    //       <p className='logo_text'> UBC PRODUCT MANAGEMENT CLUB</p>
-    //     </div>
-    //     <div className='contacts'>
-    //       <a className='email_link' href='team@ubcpm.club'>
-    //         <MdEmail /> team@ubcpm.club
-    //       </a>
-    //       <ul className='footer_links'>
-    //         <li>
-    //           <a target='_blank' href='https://www.facebook.com/ubcpmc'>
-    //             <BsFacebook />
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a target='_blank' href='https://www.instagram.com/ubcpmc/'>
-    //             <BsInstagram />
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a
-    //             target='_blank'
-    //             href='https://www.linkedin.com/company/ubc-product-management-club/'
-    //           >
-    //             <BsLinkedin />
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a
-    //             target='_blank'
-    //             href='https://www.youtube.com/channel/UCkcrUv-e7GndWtFdihI9FGQ/videos'
-    //           >
-    //             <BsYoutube className='fa_icons' />
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </footer>
-    // </div>
   );
 }
 
