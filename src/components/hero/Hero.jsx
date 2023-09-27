dec8a4597b7b37f195faba43bac7b6b83eb86b8c
Minh Au <auduongminh2004@gmail.com>
2023-09-27 13:55:09 -0700

    temporary fix to hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,32 +6,34 @@ import ScrollDown from '../../assets/scroll_down.png';
 
 function Hero() {
   return (
-    <div className='mx-[2.19rem] xl:flex-wrap xl:flex xl:justify-center xl:items-center'>
-      <div className='hero_text max-w-[19.5rem] mt-[4rem] md:max-w-full'>
-        <h1 className='text-[2.75rem] font-[600] xl:text-[2.6rem]'>
-          Explore Product
-        </h1>
-        <h1 className='text-[2.75rem] font-[600] xl:text-[2.6rem]'>
-          Management with us.
-        </h1>
-        <div className='mt-[3.75rem] space-y-[10rem]'>
-          <a href='https://tally.so/r/mVQ4Gj' target="_blank">
-            <PrimaryButton className='primary'>Join the club</PrimaryButton>
-          </a>
-          <a href='https://tally.so/r/3qD2MG' target="_blank" className='md:ml-[0.75rem]'>
-            <SecondaryButton className='mt-[1.94rem]'>
-              Volunteer as a PM
-            </SecondaryButton>
-          </a>
+    <div className='mx-[2.19rem] md:flex-col md:justify-center md:items-center'>
+      <div className='md:flex-wrap md:flex xl:justify-center md:items-center'>
+        <div className='hero_text max-w-[19.5rem] mt-[4rem] md:max-w-full'>
+          <h1 className='text-[2.75rem] font-[600] md:text-[2.0rem] xl:text-[2.6rem]'>
+            Explore Product
+          </h1>
+          <h1 className='text-[2.75rem] font-[600] md:text-[2.0rem] xl:text-[2.6rem]'>
+            Management with us.
+          </h1>
+          <div className='mt-[3.75rem] space-y-[10rem]'>
+            <a href='https://tally.so/r/mVQ4Gj' target="_blank">
+              <PrimaryButton className='primary'>Join the club</PrimaryButton>
+            </a>
+            <a href='https://tally.so/r/3qD2MG' target="_blank" className='md:ml-[0.75rem]'>
+              <SecondaryButton className='mt-[1.94rem]'>
+                Volunteer as a PM
+              </SecondaryButton>
+            </a>
+          </div>
         </div>
+        <img
+          className='hidden md:ml-[1rem] md:block md:w-[20rem] lg:w-[25rem] xl:block xl:w-[30rem] xl:ml-[2rem]'
+          src={HeroImg}
+          alt='hero'
+        />
       </div>
-      <img
-        className='hidden xl:block xl:w-[30rem] xl:ml-[2rem]'
-        src={HeroImg}
-        alt='hero'
-      />
 
-      <div className='mt-[4.19rem] xl:ml-4rem xl:mr-[50rem]'>
+      <div className='block mt-[4.19rem] xl:ml-4rem xl:mr-[50rem]'>
         <p className='font-[400] color-[#FFF]'>Scroll to learn more</p>
         <img
           className='w-[2.5625rem] mt-[1.06rem]'
