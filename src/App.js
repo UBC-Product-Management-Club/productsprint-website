2dc8e6db4c354491e81c1354e825dd20387f2dc0
Gordon Cheung <georgehwc@gmail.com>
2024-09-08 15:06:34 -0700

    add redirect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import React from 'react';
+// import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
 
-import Header from './components/header/Header';
-import Hero from './components/hero/Hero';
-import About from './components/about/About';
-import Program from './components/program/Program';
-import Highlights from './archive/highlights/Highlights';
-import Footer from './components/footer/Footer';
-import Team from './components/team/Team';
-import ScrollToTop from './components/utils/ScrollToTop';
+// import Header from "./components/header/Header";
+// import Hero from "./components/hero/Hero";
+// import About from "./components/about/About";
+// import Program from "./components/program/Program";
+// import Highlights from "./archive/highlights/Highlights";
+// import Footer from "./components/footer/Footer";
+// import Team from "./components/team/Team";
+// import ScrollToTop from "./components/utils/ScrollToTop";
 
 function App() {
+  useEffect(() => {
+    window.location.href = "https://ubcpmc.com";
+  }, []);
+
   return (
     <>
-      <Header />
+      {/* <Header />
       <ScrollToTop>
         <Routes>
           <Route
-            path='/'
+            path="/"
             element={
               <>
                 <Hero />
@@ -27,10 +31,10 @@ function App() {
               </>
             }
           />
-          <Route path='/execs' element={<Team />} />
+          <Route path="/execs" element={<Team />} />
         </Routes>
       </ScrollToTop>
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
